939e786504842673ea1d058a3a755fb8704b07a9
DESKTOP-4OOMRTG\soldesk <rlawldud1112@gmail.com>
2026-04-02 12:06:08 +0900

    [feat] calenderC.java 를 index_cal.jsp에 연결

--- src/main/java/com/devdesk/pj/calendar/calenderC.java
+++ src/main/java/com/devdesk/pj/calendar/calenderC.java
@@ -1,15 +1,20 @@
 package com.devdesk.pj.calendar;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "calenderC", value = "/calender")
 public class calenderC extends HttpServlet {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+    //index_cal.jsp로 이동
+        request.getRequestDispatcher("calendar/index_cal.jsp").forward(request,response);
+
     }
 
     public void destroy() {
