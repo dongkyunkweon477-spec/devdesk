3ee509ee12db8f4f32d078f307f600857b6c1076
urpgz69 <leesangjun528@gmail.com>
2026-04-06 18:08:24 +0900

    feat 기업 상세페이지 관련 통계 메서드 추가

--- src/main/java/com/devdesk/pj/review/ReviewDAO.java
+++ src/main/java/com/devdesk/pj/review/ReviewDAO.java
@@ -6,6 +6,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class ReviewDAO {
     static final ReviewDAO REVIEW_DAO = new ReviewDAO();
@@ -168,4 +171,30 @@ public class ReviewDAO {
         }
         return review;
     }
+
+    public Map<String, Object> getCompanyStats(int companyId) {
+        String sql = "select count (*) as total_count," +
+                "round (avg(r_difficulty),1) as avg_difficulty, round(count(case when r_result = 'pass' then 1 end)* 100.0 / count(*), 1) as pass_rate" +
+                "from review where r_company_id = ?";
+        Map<String, Object> stats = new HashMap<>();
+        try (Connection con = DBManager_new.connect();
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
+            pstmt.setInt(1, companyId);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    stats.put("totalCount", rs.getInt("total_count"));
+                    stats.put("avgDifficulty", rs.getDouble("avg_difficulty"));
+                    stats.put("passRate", rs.getDouble("pass_rate"));
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return stats;
+
+    }
+
+
 }
