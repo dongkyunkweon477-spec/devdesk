5d137cd8b3d979e7c1b70ae71a378daec6404851
kimseonmin <rlatjsals1221@gmail.com>
2026-04-06 17:31:30 +0900

    feat comment 댓글 등록 기능 추가

---
+++ src/main/java/com/devdesk/pj/Comment/CommentDAO.java
@@ -0,0 +1,82 @@
+package com.devdesk.pj.Comment;
+
+import com.devdesk.pj.main.DBManager_new;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class CommentDAO {
+    public static void addComment(HttpServletRequest request) {
+        Connection con = null;
+        PreparedStatement ps = null;
+        String sql = "insert into comments (c_comments_id, b_board_id, member_id, c_content, c_created_date) " +
+                "values (seq_comments.nextval, ?, ?, ?, sysdate)";
+
+        try {
+            System.out.println("board_id: [" + request.getParameter("board_id") + "]");
+            System.out.println("member_id: [" + request.getParameter("member_id") + "]");
+            System.out.println("content: [" + request.getParameter("content") + "]");
+            request.setCharacterEncoding("UTF-8");
+
+            con = DBManager_new.connect();
+            ps = con.prepareStatement(sql);
+
+            ps.setInt(1, Integer.parseInt(request.getParameter("board_id")));
+            ps.setInt(2, Integer.parseInt(request.getParameter("member_id")));
+            ps.setString(3, request.getParameter("content"));
+
+            int result = ps.executeUpdate();
+            if (result == 1) {
+                System.out.println("add success");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DBManager_new.close(con, ps, null);
+        }
+
+    }
+
+    public static ArrayList<CommentVO> getComment(HttpServletRequest request, int boardId) {
+
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        ArrayList<CommentVO> comments = new ArrayList<>();
+
+        String sql = "SELECT * FROM comments WHERE b_board_id = ? ORDER BY c_created_date ASC";
+
+        try {
+            con = DBManager_new.connect();
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, boardId);
+
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                CommentVO c = new CommentVO();
+                c.setComments_id(rs.getInt("c_comments_id"));
+                c.setMember_id(rs.getInt("member_id"));
+                c.setContent(rs.getString("c_content"));
+                c.setCreated_date(String.valueOf(rs.getDate("c_created_date")));
+
+                comments.add(c);
+            }
+
+            System.out.println("댓글 개수: " + comments.size());
+            request.setAttribute("commentList", comments);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DBManager_new.close(con, ps, rs);
+        }
+
+        return comments;
+    }
+
+}
--- src/main/webapp/board/boardDetail.jsp
+++ src/main/webapp/board/boardDetail.jsp
@@ -1,8 +1,11 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html>
 <head>
     <title>Title</title>
     <link rel="stylesheet" href="css/board-detail.css">
+    <link rel="stylesheet" href="css/comment.css">
 </head>
 <body>
 <div class="board-container">
@@ -48,6 +51,55 @@
             <button class="delete-btn" onclick="deleteBoard(${board.board_id})">삭제</button>
         </div>
     </c:if>
+
+    <div class="comment-section">
+        <hr class="comment-divider">
+        <h3>댓글</h3>
+
+        <c:if test="${not empty sessionScope.user}">
+            <div class="comment-form">
+                <form action="comment_add" method="post">
+                    <input type="hidden" name="board_id" value="${board.board_id}">
+                    유저: ${sessionScope.user} <br>
+                    member_id1: ${sessionScope.user.member_id} <br>
+                    <input type="hidden" name="member_id" value="${sessionScope.user.member_id}">
+                    <div class="comment-input-wrapper">
+                        <textarea name="content" placeholder="댓글을 입력하세요" required></textarea>
+                        <button type="submit">댓글 등록</button>
+                    </div>
+                </form>
+            </div>
+        </c:if>
+
+        <div class="comment-list">
+            <c:forEach var="comment" items="${commentList}">
+                <div class="comment-item">
+                    <div class="comment-info">
+                        <span class="comment-writer">사용자(ID: ${comment.member_id})</span>
+                        <span class="comment-date">${comment.created_date}</span>
+                    </div>
+                    <div class="comment-content">
+                            ${comment.content}
+                    </div>
+
+                    <c:if test="${sessionScope.user.member_id == comment.member_id}">
+                        <div class="comment-actions">
+                            <button type="button" class="c-edit-btn"
+                                    onclick="openEdit(${comment.comments_id})">수정
+                            </button>
+                            <button type="button" class="c-delete-btn"
+                                    onclick="deleteComment(${comment.comments_id})">삭제
+                            </button>
+                        </div>
+                    </c:if>
+                </div>
+            </c:forEach>
+
+            <c:if test="${empty commentList}">
+                <p class="no-comments">아직 작성된 댓글이 없습니다.</p>
+            </c:if>
+        </div>
+    </div>
 </div>
 
 <script>
